c5da751f549f1ba45121272b9aa3d05991be356f
Chris Perez <c.perez@plan-net.ch>
2019-10-24 09:15:00 +0200

    init

---
+++ local.config.js
@@ -0,0 +1,16 @@
+/****
+ * @description
+ * Set you local config here
+ * Folders etc.
+ */
+
+module.exports  = {
+     entryFileUrl: '../DistributionPackages/lsl_site/Resources/Private/Frontend/entry.js',
+     outputGeneratedDataPath: '../DistributionPackages/lsl_site/Resources/Public/Generated',
+     codeSplittingUrl: '../typo3conf/ext/lsl_site/Resources/Public/Generated/',
+     outputJsFilePath: 'Js/main.min.js',
+     outputFontsPath: 'Fonts',
+     outputAssetsPath: 'Assets',
+     outputCSSFilePath: 'Css/main.min.css',
+     localDevURL: 'http://lenz.local/en'
+}
